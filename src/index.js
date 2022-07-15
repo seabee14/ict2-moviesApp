4434be72de0ec004b612eb88e4e40e0a3f8fe138
Conor Brennan <conorbren@gmail.com>
2022-07-15 14:36:19 +0100

    Add new review support.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import UpcomingMoviesPage from "./pages/UpcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -35,6 +36,7 @@ const App = () => {
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/reviews/:id" element={<MovieReviewPage/>} />
           <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
+          <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>

9b9194d1afb807352f0b4d6120c15b5f90d323d4
Conor Brennan <conorbren@gmail.com>
2022-07-15 12:22:53 +0100

    Using react-query to manage the browser-based cache.

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import ImageList from "@material-ui/core/ImageList";
 import ImageListItem from "@material-ui/core/ImageListItem";
 import { getMovieImages } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,15 +24,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const TemplateMoviePage = ({ movie, children }) => {
-  const classes = useStyles();
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getMovieImages(movie.id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const classes = useStyles();
+    const { data , error, isLoading, isError } = useQuery(
+      ["images", { id: movie.id }],
+      getMovieImages
+    );
+  
+    if (isLoading) {
+      return <Spinner />;
+    }
+  
+    if (isError) {
+      return <h1>{error.message}</h1>;
+    }
+    const images = data.posters 
 
   return (
     <div className={classes.root}>
--- src/index.js
+++ src/index.js
@@ -7,21 +7,36 @@ import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import UpcomingMoviesPage from "./pages/UpcomingMoviesPage";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 360000,
+        refetchInterval: 360000, 
+        refetchOnWindowFocus: false
+      },
+    },
+  });
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
           <SiteHeader />      {/* New Header  */}
-      <Routes>
-        <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}
-        />
-        <Route path="/movies/:id" element={<MoviePage/>} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-        <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}
+          />
+          <Route path="/movies/:id" element={<MoviePage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+          <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
